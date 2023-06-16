95cb57afd3cd6dda1b9a09d2544e56155fb03a55
Carine <carinedcb@hotmail.com>
2023-06-15 21:50:38 -0300

    Adding routing to top and dataObj to a const

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const fs = require('fs')
 const http = require('http')
-// const { text } = require('node:stream/consumers')
-
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+const url = require('url')
 
 
+// SERVER
 
-// console.log(textIn)
-
-
-// const textOut = `this is what we know about the avocado ${textIn}\n Created on ${Date.now()}` 
-// fs.writeFileSync('./txt/output.txt', textOut)
-
-// console.log('File written')
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
 
 const server= http.createServer((req, res) => {
-	res.end('hello from the server')
+	const pathName= req.url
+	if (pathName === '/' || pathName === '/overview'){
+		res.end('this is the overview')
+	}else  if(pathName === '/product'){
+		res.end('This is the PRODUCT')
+	}else if(pathName === '/api'){
+		res.writeHead(200, {'Content-type': 'application/json'})
+		res.end(data)
+	}else{
+		res.writeHead(404, { 
+			'Content-type': 'text/html',
+			'my-own-header': 'hello-world'
+		})
+		res.end('<h1>Page not found</h1>')
+	}
 })
 
 server.listen(8000, '127.0.0.1', () => {
